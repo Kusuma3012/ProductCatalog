9b80ff6081679a9b1c707a602882dfba28551fdf
kusuma <sk6426724@gmail.com>
2025-06-22 20:24:06 +0530

    Initial commit

---
+++ src/main/java/com/productcatolg/demo/ProductcatlogApplication.java
@@ -0,0 +1,13 @@
+package com.productcatolg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcatlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductcatlogApplication.class, args);
+	}
+
+}
